93b105666eb687f496b86b3485da5293bbb634fb
mokasin <mok4sin@googlemail.com>
2012-06-24 13:10:42 +0200

    Making Soundmanager2 a submodule.

---
+++ web/assets/js/soundmanager-settings.js
@@ -0,0 +1,6 @@
+soundManager.url = 'assets/soundmanager2/swf/';
+//soundManager.flashVersion = 9; // optional: shiny features (default = 8)
+soundManager.useHTML5Audio = true;
+soundManager.preferFlash = true;
+soundManager.debugMode = true;
+//soundManager.audioFormats.mp3.required = false;
