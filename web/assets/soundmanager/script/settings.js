516d24a361d7139ee46432c4ef47f74432a7b19d
mokasin <mok4sin@googlemail.com>
2012-06-24 13:05:05 +0200

    Deleting Soundmanager.

--- web/assets/soundmanager/script/settings.js
+++
@@ -1,6 +0,0 @@
-soundManager.url = 'assets/soundmanager/swf/';
-//soundManager.flashVersion = 9; // optional: shiny features (default = 8)
-soundManager.useHTML5Audio = true;
-soundManager.preferFlash = true;
-soundManager.debugMode = true;
-//soundManager.audioFormats.mp3.required = false;
