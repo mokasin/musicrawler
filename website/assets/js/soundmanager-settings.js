64513c55acc1fe0087b128182fab0f4124c91917
mokasin <mok4sin@gmail.com>
2012-07-14 23:08:55 +0200

    Correcting soundManager.url

--- website/assets/js/soundmanager-settings.js
+++ website/assets/js/soundmanager-settings.js
@@ -1,4 +1,4 @@
-soundManager.url = 'assets/js/SoundManager2/';
+soundManager.url = '/assets/js/SoundManager2/';
 //soundManager.flashVersion = 9; // optional: shiny features (default = 8)
 soundManager.useHTML5Audio = true;
 soundManager.preferFlash = true;
